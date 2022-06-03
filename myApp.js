f318a9f2aa357a32ce9dd3ec24121092e5eef07a
Victoria Lojek <victoria.lojek@web.de>
2022-06-03 15:40:48 +0200

    Update

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 let express = require("express")
 let app = express()
-console.log("Hello World")
 
-module.exports = app
+app.get("/", (req, res) => {
+  res.send("Hello Express")
+})
+
+console.log("Hello World!!")
